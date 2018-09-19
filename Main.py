00581dbed140d610947dc9088536a70817620ca7
Brandon Stewart <stewartb2@gmail.com>
2018-09-19 15:23:36 -0400

    Fix calling getData()

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from Data import *
 
 def main():
 	#	Each of these data containers looks like ([X,784]: data, [X]: label) where data \in (0,1)
-	trDat, valDat, teDat = Data.getData("mnist.pkl.gz")
+	trDat, valDat, teDat = getData("mnist.pkl.gz")
 
 	n = Network(784, 10, [75, 50, 50])
 	n.Test(*teDat)
